f8b87614b66e1f5dd319c63c86ebafc8ddf57f20
vasiliev-alexey <alex_v81@MAIL.RU>
2021-10-05 16:21:58 +0300

    add settings for react

---
+++ packages/ch-6/src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+import StarRating from './components/StarRating';
+function app(): React.ReactElement {
+  return <StarRating />;
+}
+
+export default app;
---
+++ packages/ch-6/src/components/StarRating.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const StarRating: React.FC = () => {
+  return (
+    <>
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="red" />
+      <FaStar color="grey" />
+      <FaStar color="grey" />
+      <p>sss</p>
+    </>
+  );
+};
+
+export default StarRating;
